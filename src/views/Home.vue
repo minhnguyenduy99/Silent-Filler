5f03b16a4f9fe131af0d8435034e325466974efb
minhnguuyenduy <minhnguyenduy99@gmail.com>
2020-06-10 09:35:26 +0700

    Add home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="image-container">
+      <b-img class="image-container__image" src="@/assets/background.jpg" />
+    </div>
+    <div class="main-content__container d-flex justify-content-center align-items-center">
+      <div class="main-content__area">
+        <h1 class="main-content__game-title display-3 text-light">The name of the game</h1>
+        <h4 class="main-content__game-description text-light">Description of the game</h4>
+        <div class="d-flex justify-content-center mt-5">
+          <b-button class="mr-3 py-3 px-4" size="lg" variant="success">Login</b-button>
+          <b-button class="ml-3 py-3 px-4" size="lg" variant="outline-info">About</b-button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
+
+<style scoped lang="scss">
+
+.image-container {
+  position: relative;
+  z-index: 1;
+
+  & > &__image {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+  }
+}
+
+.main-content {
+
+  &__container {
+    position: relative;
+    z-index: 2;
+    height: 100vh;
+    width: 100vw;
+  }
+}
+</style>
