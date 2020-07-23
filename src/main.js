f18813b7b05de227d79ea760901f2b685cc38faf
minhnguuyenduy <minhnguyenduy99@gmail.com>
2020-07-23 09:47:57 +0700

    Remove vue-head package

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import 'mutationobserver-shim'
 import './plugins/bootstrap-vue'
 import './plugins/auth0'
 import Vue from 'vue'
-import VueHead from 'vue-head'
 import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
@@ -12,7 +11,6 @@ import store from './store'
 
 Vue.config.productionTip = false
 
-Vue.use(VueHead)
 Vue.use(VueAxios, axios)
 
 new Vue({
